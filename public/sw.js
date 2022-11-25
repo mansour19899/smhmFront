412db42db2a7cd50033a1f61527bc0f995ee1f9c
mansour <smm.2020@gmail.com>
2022-11-25 03:42:27 -0500

    starting

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', function (event) {
+     self.skipWaiting();
+});
+
+self.addEventListener('activate', function (event) {});
+
+self.addEventListener('fetch', function (event) {});
